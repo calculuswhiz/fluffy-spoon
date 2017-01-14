7bfe00d1fbf80170268e327c83b0bc286991c0dc
calculuswhiz <calculuswhiz@gmail.com>
2017-01-14 02:17:08 -0500

    All-angle aiming.

--- GUIv2.py
+++ GUIv2.py
@@ -7,7 +7,7 @@ from PyQt5 import uic, QtCore
 from PyQt5.QtCore import Qt, QObject, pyqtSignal
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import (QWidget, QApplication,
-        QGridLayout, QMainWindow, QStatusBar,
+        QGridLayout, QMainWindow, QStatusBar, QTableWidgetItem,
         QLabel, QButtonGroup, QPushButton, QRadioButton, QLineEdit, QSpinBox)
 
 import Xlib
@@ -79,6 +79,8 @@ class MW(QMainWindow, RetoolGUI.Ui_MainWindow):
     def solve_tank(self):
         # Sanity check.
         shotstring=""
+        
+        self.calcTable.setSortingEnabled(False)
 
         # Earth:
         gc = 9.529745042492918
@@ -86,7 +88,9 @@ class MW(QMainWindow, RetoolGUI.Ui_MainWindow):
         # hover:
         if(self.radHover.isChecked()):
             hc = 3.660218073939021
-        hc = 7.313616408030493*int(self.radBigHover.isChecked())
+        else:
+            hc = 7.313616408030493*int(self.radBigHover.isChecked())
+        
         if(hc!=0):
             shotstring = "(HOVER)"
         # boomer:
@@ -99,16 +103,13 @@ class MW(QMainWindow, RetoolGUI.Ui_MainWindow):
 
         dx = self.wbox.value()
         dy = self.hbox.value()
-        theta = self.anglebox.value()
-        if self.radDig.isChecked():
-            theta = -theta+360
-            dy = -dy
-            shotstring="(TUNNELER)"
-        theta = rad(theta)
+        barrel = self.anglebox.value()
         w = self.windbox.value()
         # hang = int(self.radHover.isChecked())
-            
-        flags = ""
+        
+        if self.radDig.isChecked():
+            # dy = -dy
+            pass
         
         # Solve these equations:
         # x=v_0*cos(theta)*t
@@ -116,42 +117,59 @@ class MW(QMainWindow, RetoolGUI.Ui_MainWindow):
         #   Hover:     hc*v_0*cos(theta)
         #   Wind:      .5*wf*w*(t**2)
         #   Boomer:    -.5*bc*v_0*cos(theta)*(t**2)
-        
+            
         # y=v_0*sin(theta)*t-1/2*gc*(t**2)
-        if v_0 is None:
-            diffx = 20
-            vat = -1
-            for n in range(1,111):
-                t = quadform(gc/2, -n*sin(theta), dy)[0]
-                # if t<0:
-                #     continue
-                x = n*cos(theta)*t + .5*wf*w*t**2 + hc*n*cos(theta) - .5*bc*n*cos(theta)*t**2
-                # print(fabs(x-dx))
-                if fabs(x-dx) < fabs(diffx):
-                    # Debug:
-                    # print(str(n)+":\t", round(x), round(dx), round(fabs(dx-x)))
-                    diffx = dx-x
-                    vat = n
-            catstr = ""
-            if(vat > 100 or diffx > 5):
-                catstr = "!"
-            self.vbox.setText(catstr + str(round(vat,0))+","+str(round(diffx,1))+"px" + catstr)
-            if vat<0:
-                call(["notify-send", "Error:", "Bad Value"])
-            else:
-                call(["notify-send", "Power:", str(round(vat,0))+catstr+" "+shotstring])
+        pairlist=[]
+        # barrel = 15
+        for theta in range(0,360):
+            inversion = 1
+            if self.radDig.isChecked():
+                theta = -theta+360
+                dy = -self.hbox.value()
+                inversion = -2
+        
+            theta = rad(theta)
+            if v_0 is None:
+                diffx = 20
+                vat = -1
+                for n in range(1,101):
+                    t = quadform(gc/2, -n*sin(theta), (dy - barrel *sin(theta)))[0]
+                    x = n*cos(theta)*t + .5*wf*w*t**2 + hc*n*cos(theta) - .5*bc*n*cos(theta)*t**2
+                    # print(fabs(x-dx))
+                    # if fabs(x-(dx)) < fabs(diffx):
+                    if fabs(x-(dx - barrel * cos(theta))) < fabs(diffx):
+                        # Debug:
+                        # print(str(n)+":\t", round(x), round(dx), round(fabs(dx-x)))
+                        # diffx = (dx) -x
+                        diffx = (dx - barrel * cos(theta) ) -x
+                        vat = n
                 
-        else:
-            print("Error. Broken.")
-            self.vbox.setText(str(-1.0))
-
+                theta = deg(theta)
+                if self.radDig.isChecked():
+                    theta = -theta+360
+                
+                if(fabs(diffx)<5.0):
+                    # print(theta)
+                    pairlist.append((fabs(diffx), vat, theta))
+            else:
+                print("Error. Broken.")
+                self.vbox.setText(str(-1.0))
+        pairlist.sort()
+        self.calcTable.setRowCount(len(pairlist))
+        for i in range(0, len(pairlist)):
+            self.calcTable.setItem(i, 0, QTableWidgetItem(str(round(pairlist[i][1],0))))
+            self.calcTable.setItem(i, 1, QTableWidgetItem(str(round(pairlist[i][2],0))))
+            self.calcTable.setItem(i, 2, QTableWidgetItem(str(round(pairlist[i][0],1))))
+            print (pairlist[i])
+        self.calcTable.setSortingEnabled(True)
+            
     def keyPressEvent(self, e):
         if e.key() == Qt.Key_Escape or e.key() == Qt.Key_Q:
             self.close()
 
 if __name__ == '__main__':
     # gcproc = Popen(["gameconqueror"])
-    call(["gameconqueror"])
+    # call(["gameconqueror"])
     app = QApplication(sys.argv)
     Mainwin = MW()
     Mainwin.show()
